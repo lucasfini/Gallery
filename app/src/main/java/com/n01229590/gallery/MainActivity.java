3fd677147374204d9ee033c07805cb03f4f3dcb0
Lucas <lucasfiniello@gmail.com>
2019-11-30 17:31:51 -0500

    Added comments

--- app/src/main/java/com/n01229590/gallery/MainActivity.java
+++ app/src/main/java/com/n01229590/gallery/MainActivity.java
@@ -1,16 +1,14 @@
 package com.n01229590.gallery;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.os.Bundle;
+
+// TODO Implement these imports
 import android.widget.AdapterView;
 import android.widget.Gallery;
-
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.Toast;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -18,7 +16,9 @@ import android.view.ViewGroup;
 public class MainActivity extends AppCompatActivity {
 
 
-    private Integer[] images = {R.drawable.image1, R.drawable.image2, R.drawable.image3, R.drawable.image4, R.drawable.image5};
+    // Variable for ImageView.
+    private ImageView sImage;
+
 
 
     @Override
@@ -26,65 +26,92 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // TODO 1.3 Create two variables, one for Gallery and one for ImageView. Attach appropriate ids from layout.
         Gallery ims = (Gallery) findViewById(R.id.gallery);
+        sImage = (ImageView) findViewById(R.id.imageView);
 
-        ims.setAdapter(new ImageAdapter(this));
+        ims.setSpacing(1);
 
+        // TODO 1.4 Create ImageAdaptor class which extends BaseAdaptor. This will work as a bridge between AdapterView and the data source that gets inputed.
+        final ImageAdapter gImageAdapter= new ImageAdapter(this);
+        ims.setAdapter(gImageAdapter);
+
+       // TODO 1.5 This will allow a user to click on an image.
         ims.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                Toast.makeText(getApplicationContext(), "Image " + arg2, Toast.LENGTH_SHORT).show();
-
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                // show the selected Image
+                sImage.setImageResource(gImageAdapter.images[position]);
             }
         });
-
-
     }
 
 
 
-    public class ImageAdapter extends BaseAdapter {
-        private Context context;
-        int imageBackground;
+}
 
+
+// TODO 1.4.1 Fill ImageAdaptor Class with these methods: getCount(), getItem(), getItemId(), getView.
+    class ImageAdapter extends BaseAdapter
+    {
+        private Context Mcontext;
+
+       //Constructor
         public ImageAdapter(Context context) {
 
-            this.context = context;
+            Mcontext = context;
         }
 
+        // Gets length of array.
         @Override
         public int getCount() {
 
             return images.length;
         }
 
+        // Gets the data associated with the specified position in the list.
         @Override
-        public Object getItem(int arg0) {
+        public Object getItem(int pos) {
 
-            return arg0;
+            return pos;
         }
 
+        // Returns the position within the adaptors data set.
         @Override
-        public long getItemId(int arg0) {
+        public long getItemId(int pos) {
 
-            return arg0;
+            return pos;
+        }
+        // This allows the developer to inflate the view into the proper layout parameters.
+        public View getView(int index, View view, ViewGroup viewGroup)
+        {
+
+            ImageView i = new ImageView(Mcontext);
+
+            i.setImageResource(images[index]);
+            i.setLayoutParams(new Gallery.LayoutParams(200, 200));
+
+            i.setScaleType(ImageView.ScaleType.FIT_XY);
+
+
+
+
+            return i;
         }
 
 
-        @Override
-        public View getView(int arg0, View arg1, ViewGroup arg2) {
+        // TODO 1.1 Add Images to res/drawable to fill gallary.
+        //TODO 1.2 Create array for images and grab images from drawable. Example below.
 
-            ImageView imageView = new ImageView(context);
-            imageView.setImageResource(images[arg0]);
-            return imageView;
-        }
-
+       //Array of images.
+        public Integer[] images = {
+                R.drawable.image1,
+                R.drawable.image2,
+                R.drawable.image3,
+                R.drawable.image4,
+                R.drawable.image5
+        };
 
 
 
     }
 
-}
